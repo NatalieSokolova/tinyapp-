a79f4a0350de023b4aa4e9f576a217fdd19e050d
NatalieSokolova <nataliesklv@gmail.com>
2020-07-06 23:56:43 +0000

    added function that redirects to long URL

--- express_server.js
+++ express_server.js
@@ -40,6 +40,12 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', webPage);
 });
 
+// redirects to longURL
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+})
+
 app.post('/urls', (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   res.send('Ok'); // Respond with 'Ok' (we will replace this)
