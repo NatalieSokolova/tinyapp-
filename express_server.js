257790528e137dfe808a3c9dddde83d003aa240d
NatalieSokolova <nataliesklv@gmail.com>
2020-07-06 23:35:53 +0000

    added function that generates a random string

--- express_server.js
+++ express_server.js
@@ -4,10 +4,19 @@ const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+// generates a "unique" shortURL
+const generateRandomString = () => {
+  return Math.random().toString(36).slice(7);
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -18,13 +27,22 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  let webPage = { urls: urlDatabase };
+  res.render('urls_index', webPage);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars);
+  let webPage = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', webPage);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/hello', (req, res) => {
