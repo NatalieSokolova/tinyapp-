27a92cb7a2c47f9ef914d0084df7caa479625819
NatalieSokolova <nataliesklv@gmail.com>
2020-07-08 18:21:24 +0000

    added POST /register req that checks if user exists, if no - adds to users db and sets cookie for id

--- express_server.js
+++ express_server.js
@@ -15,16 +15,17 @@ const urlDatabase = {
 
 // USERS database
 const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
+  // example users for debugging
+//   "userRandomID": {
+//     id: "userRandomID", 
+//     email: "user@example.com", 
+//     password: "purple-monkey-dinosaur"
+//   },
+//  "user2RandomID": {
+//     id: "user2RandomID", 
+//     email: "user2@example.com", 
+//     password: "dishwasher-funk"
+//   }
 }
 
 // generates a "unique" string 
