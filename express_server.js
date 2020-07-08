fc1517ebd0ac12d2faa4392e1572163bddc70ca6
NatalieSokolova <nataliesklv@gmail.com>
2020-07-08 22:40:14 +0000

    debugged logout

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser');
@@ -14,21 +14,9 @@ const urlDatabase = {
 };
 
 // USERS database
-const users = { 
-  // example users for testing
-//   "userRandomID": {
-//     id: "userRandomID", 
-//     email: "user@example.com", 
-//     password: "purple-monkey-dinosaur"
-//   },
-//  "user2RandomID": {
-//     id: "user2RandomID", 
-//     email: "user2@example.com", 
-//     password: "dishwasher-funk"
-//   }
-}
+const users = {};
 
-// generates a "unique" string 
+// generates a "unique" string
 const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
 };
@@ -41,7 +29,7 @@ const findUserByEmail = (email) => {
     }
   }
   return false;
-}
+};
 
 const addNewUser = (name, email, password) => {
   const userID = generateRandomString();
@@ -54,22 +42,11 @@ const addNewUser = (name, email, password) => {
   };
   users[userID] = newUser;
   return userID;
-}
-
-// homepage message
-// app.get('/', (req, res) => {
-//   res.send('Hello!');
-// });
-
-// app.get('/urls.json', (req, res) => {
-//   res.json(urlDatabase);
-// });
+};
 
 //login
-
 app.get('/login', (req, res) => {
   const templateVars = {
-    //username: null,
     user: users[req.cookies['user_id']]
   };
   res.render('login', templateVars);
@@ -81,55 +58,41 @@ app.post('/login', (req, res) => {
   if (!user.email) {
     return res.status(403).send('Sorry, we can\'t find a user with matching email address!');
   } else {
-    if (!user.password) {
+    if (req.body.password !== user.password) { // checks if the pw input matches registered pw
       return res.status(403).send('Sorry, your password is incorrect!');
-    };
-  };
-    const userID = user.id;
-    console.log('userID: ', userID);
-    // set a user_id cookie containing the user's ID
-    res.cookie('user_id', userID);
-    res.redirect('/urls');
-  // const username = req.body.username;
-  // res.cookie('username', username);
-  // // console.log('username:', username);
-  // // console.log('body: ', req.body);
-  // res.redirect('/urls');
+    }
+  }
+  const userID = user.id;
+  console.log('userID: ', userID);
+  // set a user_id cookie containing the user's ID
+  res.cookie('user_id', userID);
+  res.redirect('/urls');
 });
 
 //logout
 app.post('/logout', (req, res) => {
-  // const username = req.body.username;
-  // res.clearCookie('username');
+  //const user = users[req.cookies['user_id']];
+  res.clearCookie('user_id');
   // for (let urls in urlDatabase) {
   //   delete urlDatabase[urls];
   // }
-  // res.redirect('/urls');
-  const user = users[req.cookies['user_id']];
-  res.clearCookie('user_id');
-  for (let urls in urlDatabase) {
-    delete urlDatabase[urls];
-  }
   res.redirect('/urls');
 });
 
 // list of all URLs
 app.get('/urls', (req, res) => {
-  //let webPage = { urls: urlDatabase };
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies['username'],
     user: users[req.cookies['user_id']]
-  }
+  };
   res.render('urls_index', templateVars);
 });
 
 // newURL form
 app.get('/urls/new', (req, res) => {
   const templateVars = {
-    username: req.cookies['username'],
     user: users[req.cookies['user_id']]
-  }
+  };
   res.render('urls_new', templateVars);
 });
 
@@ -137,40 +100,36 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   if (urlDatabase[shortURL]) { // if exists - render the page
-  const templateVars = {
-    shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies['username'],
-    user: users[req.cookies['user_id']]
-  }
-  res.render('urls_show', templateVars);
+    const templateVars = {
+      shortURL,
+      longURL: urlDatabase[req.params.shortURL],
+      user: users[req.cookies['user_id']]
+    };
+    res.render('urls_show', templateVars);
   } else { // if doesn't exist - redirect to urls list page
-  res.redirect('/urls');
+    res.redirect('/urls');
   }
 });
 
 // redirects to longURL website
 app.get('/u/:shortURL', (req, res) => {
-  console.log(req.params.shortURL) // short URL(found in address bar)
+  console.log(req.params.shortURL); // short URL(found in address bar)
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 // adds new URL to the urlDatabase
 app.post('/urls', (req, res) => {
-  // console.log('POST req.body: ', req.body); // found in form
-  // console.log('longURL: ', longURL);
-  // console.log('database: ', urlDatabase); 
-    const shortURL = generateRandomString();
-    const longURL = req.body.longURL;
-    urlDatabase[shortURL] = longURL;
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
   
   res.redirect(`/urls/${shortURL}`); // redirects to the 'TinyURL/Short URL' webpage after clicks Submit;
 });
 
 // deletes a URL from urlDatabase
 app.post('/urls/:shortURL/delete', (req, res) => {
-  // console.log('shortURL: ', req.params.shortURL) 
+  // console.log('shortURL: ', req.params.shortURL)
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
@@ -189,11 +148,10 @@ app.post('/urls/:shortURL', (req, res) => {
 // takes user to registration form
 app.get('/register', (req, res) => {
   const templateVars = {
-    username: null,
     user: users[req.cookies['user_id']]
   };
   res.render('register', templateVars);
-})
+});
 
 app.post('/register', (req, res) => {
   //const user = generateRandomString();
@@ -220,11 +178,8 @@ app.post('/register', (req, res) => {
   } else {
     res.status(401).send('Error: email already exists!');
   }
-})
+});
 
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>Hello <b>World</b></body></html>\n');
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
